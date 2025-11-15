2ce247e888caa360751fca64b59df8e7fbc0f664
Olivia Coumans <livie7c@gmail.com>
2025-11-15 11:48:04 +0100

    feat: replace example app with vite react-ts app

--- example-app/eslint.config.js
+++ example-app/eslint.config.js
@@ -1,3 +1,17 @@
 import { eslintConfig } from '@oliviac-tutorial-demo/configs';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
-export default eslintConfig;
+export default [
+  ...eslintConfig,
+  {
+    plugins: {
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
+  {
+    ignores: ['dist'],
+  },
+];
